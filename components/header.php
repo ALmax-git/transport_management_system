93e57cf6ad6ac190056276ad954b8b80f7d53a02
ALmax-git <alimustaphashettima@gmail.com>
2024-10-18 15:39:04 +0100

    Happy Coding

--- components/header.php
+++ components/header.php
@@ -16,7 +16,7 @@
               <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
           </nav>
 
-          <a class="btn-getstarted" href="#">Get Started</a>
+          <a class="btn-getstarted" href="./auth.php">Get Started</a>
 
       </div>
   </header>
