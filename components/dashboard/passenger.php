ab4f833018dc00d2d075a2c7578958803b366d21
ALmax-git <alimustaphashettima@gmail.com>
2024-11-13 05:30:17 +0100

    Hello World

--- components/dashboard/passenger.php
+++ components/dashboard/passenger.php
@@ -1,6 +1,16 @@
 <div class="card" style="margin:auto;">
 <div class="row">
 <form class="form" action="./app/mainController.php" method="post">
+
+   <label class="form-label-wrapper">
+     <p class="form-label">Title</p>
+     <select class="form-input"  name="title">
+      <option value="Mr.">Mr.</option>
+      <option value="Mrs">Mrs</option>
+      <option value="Dr.">Dr.</option>
+      <option value="">Others or non</option>
+     </select>
+   </label>
    <label class="form-label-wrapper">
      <p class="form-label">Name</p>
      <input class="form-input" type="text" name="name"  placeholder="Enter passenger name" required>
@@ -18,8 +28,8 @@
      <input class="form-input" type="tel" name="first_contact_number" placeholder="Enter first contact number" required>
    </label>
    <label class="form-label-wrapper">
-     <p class="form-label">Age</p>
-     <input class="form-input" type="number" name="age" placeholder="Enter passenger age">
+     <p class="form-label">Date of Birth</p>
+     <input class="form-input" type="date" name="age" placeholder="Enter passenger age">
    </label>
    <label class="form-label-wrapper">
      <p class="form-label">Gender</p>
