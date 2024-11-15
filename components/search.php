7ccef44a43a0fe909d4e00a3fdc76c03d6e1ab1c
ALmax-git <alimustaphashettima@gmail.com>
2024-11-15 11:31:06 +0100

    Hello World

--- components/search.php
+++ components/search.php
@@ -58,7 +58,28 @@
               </div>
 
               <div class="col-lg-5 order-1 order-lg-2 hero-img" data-aos="zoom-out">
-                  <img src="assets/img/hero-img.svg" class="img-fluid mb-3 mb-lg-0" alt="">
+              <div id="carouselExampleAutoplaying" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/img/about.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/service-2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/img/service-1.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+                  <!--<img src="" class="img-fluid mb-3 mb-lg-0" alt="">-->
               </div>
 
           </div>
