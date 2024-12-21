3af308cbf7f8e9f652267a079a8013721ea468a9
ALmax-git <alimustaphashettima@gmail.com>
2024-12-21 12:31:22 +0100

    ALmax

--- index.php
+++ index.php
@@ -209,47 +209,22 @@
                                 </div>
                             </div><!-- End Info Item -->
 
-                            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="500">
+                            <div class="info-item d-flex" data-aos="fade-up" data-aos-delay="400">
+                                <i class="bi bi-whatsapp flex-shrink-0"></i>
+                                <div>
+                                    <h3>Whatsapp</h3>
+                                    <p>+23481 6514 1519</p>
+                                </div>
+                            </div><!-- End Info Item -->
+                            <a href="./?contact_us=true" class="info-item d-flex" data-aos="fade-up" data-aos-delay="500">
                                 <i class="bi bi-envelope flex-shrink-0"></i>
                                 <div>
                                     <h3>Email Us</h3>
                                     <p>contact@bornoexpress.com</p>
                                 </div>
-                            </div><!-- End Info Item -->
+                            </a><!-- End Info Item -->
 
                         </div>
-
-                        <div class="col-lg-8">
-                            <form action="forms/contact.php" method="post" class="php-email-form" data-aos="fade-up" data-aos-delay="200">
-                                <div class="row gy-4">
-
-                                    <div class="col-md-6">
-                                        <input type="text" name="name" class="form-control" placeholder="Your Name" required="">
-                                    </div>
-
-                                    <div class="col-md-6 ">
-                                        <input type="email" class="form-control" name="email" placeholder="Your Email" required="">
-                                    </div>
-
-                                    <div class="col-md-12">
-                                        <input type="text" class="form-control" name="subject" placeholder="Subject" required="">
-                                    </div>
-
-                                    <div class="col-md-12">
-                                        <textarea class="form-control" name="message" rows="6" placeholder="Message" required=""></textarea>
-                                    </div>
-
-                                    <div class="col-md-12 text-center">
-                                        <div class="loading">Loading</div>
-                                        <div class="error-message"></div>
-                                        <div class="sent-message">Your message has been sent. Thank you!</div>
-
-                                        <button type="submit">Send Message</button>
-                                    </div>
-
-                                </div>
-                            </form>
-                        </div><!-- End Contact Form -->
 
                     </div>
 
@@ -257,6 +232,8 @@
 
             </section><!-- /Contact Section -->
 
+        <?php elseif (isset($_GET['contact_us']) && $_GET['contact_us'] == "true"): ?>
+            <?php include './components/contact.php'; ?>
         <?php else: ?>
 
             <?php include './components/search.php'; ?>
